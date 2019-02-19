a5aafa003b9299f9cf1502e67af9121a362b2374
U-NVIDIA.COM\mqueiroz <mqueiroz@MQUEIROZ-LT2.nvidia.com>
2019-02-19 20:19:51 -0300

    Eliminate symmetric solutions

--- nqueens.py
+++ nqueens.py
@@ -15,7 +15,7 @@
 # ☐ ♕ ☐ ☐
 
 
-import argparse, sys 
+import argparse, sys, sets 
 
 parser = argparse.ArgumentParser(description='Returns the number of ways of placing n nonattacking queens on an n X n board.')
 parser.add_argument('n', type=int, help='The size of the n x n board')
@@ -23,47 +23,81 @@ parser.add_argument('-s','--show', action="store_true", help='Display found solu
 args = parser.parse_args()
 n = args.n
 count=0
+solutions = set()
 
-def valid (j, row, p):
+
+def valid (j, row, q):
     for i in range(row):
         # There's a previous queen in the same column
-        if p[i]==j:
+        if q[i]==j:
             return False
         # There's a previous queen in a diagonal to the left
-        if p[i]+row-i == j:
+        if q[i]+row-i == j:
             return False
         # There's a previous queen in a diagonal to the right
-        if p[i]-row+i == j:
+        if q[i]-row+i == j:
             return False
     return True
 
-def print_board(p):
+def print_board(q):
     for i in range(n):
         for j in range(1,n+1):
-            if (p[i] == j):
+            if (q[i] == j):
                 print("♕"),
             else:
                 print("☐"),
         print
-    print
+    print(q)
 
-def nqueens(row,queens):
+
+def nqueens(row,q):
     global count
     
     if row == n:
         # Found
+        
+        s = tuple(q)
+        r = tuple(q[::-1])
+        f = tuple([n-q[i]+1 for i in range(n)])
+        fr = tuple(f[::-1])
+        z = [0]*n
+        for i in range(n):
+            z[q[i]-1]=i+1
+        t = tuple(z)
+        tr = tuple(z[::-1])
+        fzr = tuple([n-t[i]+1 for i in range(n)])
+
+        fzrr = tuple(fzr[::-1])
+        u = [0]*n
+        for i in range(n):
+            u[n-1-i]=n-s[i]+1
+        ur = tuple(u[::-1])
+        u = tuple(u)
+
+        
+
+        sym = set([s,r,t,f,tr,fzr,u,ur,fzrr])
+        if not solutions.isdisjoint(sym):
+            return
+        
+        solutions.add(s)
         count = count + 1
+
         if args.show:
-            print_board(queens)
+            print_board(q)
+#        for q in [s,r,t,f,tr,fzr,u,ur,fzrr]:
+#            print_board(q)
+        
+        print
         return
     for i in range(1,n+1):
-        queens[row] = i
-        if valid(i, row,queens):
-            nqueens(row+1, queens)
-    queens[row]=0
+        q[row] = i
+        if valid(i, row,q):
+            nqueens(row+1, q)
+    q[row]=0
 if n < 2:
     count = 1
 else:
-    nqueens(row=0,queens=[0]*n)
+    nqueens(row=0,q=[0]*n)
 print (count)
 
